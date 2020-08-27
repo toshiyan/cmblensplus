615f95700c44c6e5640e7496ae7c61dff886c8e9
Your Name <you@example.com>
2020-08-27 06:08:18 -0700

    binning update

--- utils/binning.py
+++ utils/binning.py
@@ -14,12 +14,55 @@ class multipole_binning:
         self.bp, self.bc = basic.aps.binning(n,[lmin,lmax],spc=spc)
 
 
-def binning(cl,mb0,mb1=None):
+def binning(cl,mb0,mb1=None,vl=None):
 
-    if mb1 is None:
-        return binning1(cl,mb0)
+    if vl is None:
+        if mb1 is None:
+            return binning1(cl,mb0)
+        else:
+            return binning2(cl,mb0,mb1)
     else:
-        return binning2(cl,mb0,mb1)
+        # binning of power spectrum with un-equal weights from variance
+        return binning_opt(cl,vl,mb0)
+
+
+def binning_opt(cl,vl,mb):
+
+    if mb.lmax > np.shape(cl)[-1] - 1:
+        sys.exit('size of mb.lmax is wrong: '+str(mb.lmax)+', '+str(np.shape(cl)[-1]-1))
+
+    if np.ndim(cl) == 1:
+        cb = binning_opt_core(cl[:mb.lmax+1],vl[:mb.lmax+1],mb)
+
+    if np.ndim(cl) == 2:
+        snmax = np.shape(cl)[0]
+        cb = np.array([ binning_opt_core(cl[i,:mb.lmax+1],vl[:mb.lmax+1],mb) for i in range(snmax)])
+    
+    if np.ndim(cl) == 3:
+        snmax = np.shape(cl)[0]
+        clnum = np.shape(cl)[1]
+        cb = np.array([[ binning_opt_core(cl[i,c,:mb.lmax+1],vl[:mb.lmax+1],mb) for c in range(clnum)] for i in range(snmax)])
+
+    return cb
+
+
+def binning_opt_core(cl,vl,mb):
+
+    cb = np.zeros(mb.n)
+    
+    for i in range(mb.n):
+        
+        b0, b1 = int(mb.bp[i]) , int(mb.bp[i+1])
+        
+        cs = cl[b0:b1]
+        wl = 1./vl[b0:b1]**2
+        
+        if np.count_nonzero(wl) > 0:
+            cb[i] = np.sum(wl[wl!=0]*cs[wl!=0]) / np.sum(wl[wl!=0])
+        else:
+            cb[i] = 0
+
+    return cb
 
 
 def binning1(cl,b):
@@ -61,13 +104,19 @@ def binning2(cl,b0,b1):
         return np.concatenate((cb0,cb1),axis=1)
 
 
-def binned_spec(mb,fcl,cn=1,doreal=True):
+def binned_spec(mb,fcl,cn=1,doreal=True,opt=False,vl=None):
     # for a given array of files, fcl, which containes real (fcl[0]) and sims (fcl[1:]), return realization mean and std of binned spectrum
     
     snmax = len(fcl)
 
     scl = np.array([np.loadtxt(fcl[i],unpack=True)[cn] for i in range(1,snmax)])
-    scb = binning(scl,mb)
+    
+    if opt and vl is None:
+        Vl = np.std(scl,axis=0)
+    else:
+        Vl = vl
+    
+    scb = binning(scl,mb,vl=Vl)
 
     mcb = np.mean(scb,axis=0)
     vcb = np.std(scb,axis=0)
@@ -75,13 +124,20 @@ def binned_spec(mb,fcl,cn=1,doreal=True):
     if doreal:
 
         ocl = np.loadtxt(fcl[0],unpack=True)[cn]
-        ocb = binning(ocl,mb)
+        ocb = binning(ocl,mb,vl=Vl)
 
-        return mcb, vcb, scb, ocb
+        if opt and vl is None:
+            return mcb, vcb, scb, ocb, Vl
+        else:
+            return mcb, vcb, scb, ocb
 
     else:
    
-        return mcb, vcb, scb
+        if opt and vl is None:
+            return mcb, vcb, scb, Vl
+        else:
+            return mcb, vcb, scb
+
 
 
 
