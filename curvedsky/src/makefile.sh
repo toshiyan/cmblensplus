2b48be7c8d4176c11f773139cd80c3a822c0d54f
Your Name <you@example.com>
2020-09-08 14:27:16 -0700

    add verbose feature for docstr

--- curvedsky/src/makefile.sh
+++ curvedsky/src/makefile.sh
@@ -12,7 +12,8 @@ link="-lhp -llenspix -lmatrix -lutils -lhealpix -lcfitsio -liomp5 -lpthread -lla
 # liomp5 and lpthread are required to use healpix
 
 # files to be compiled
+#scan="utils.f90"
 scan="utils.f90 cninv.f90 rec_lens.f90 norm_lens.f90 rec_rot.f90 norm_rot.f90 rec_tau.f90 norm_tau.f90 rec_src.f90 norm_src.f90 delens.f90 bispec.f90"
 
-source ../../sh/compile.sh ${1}
+source ../../sh/compile.sh ${1} ${2}
 
