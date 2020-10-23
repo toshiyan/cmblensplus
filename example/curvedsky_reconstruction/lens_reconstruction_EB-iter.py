3de25ade52343bc823525de2f60e3bba84edf4bb
Your Name <you@example.com>
2020-10-23 07:36:31 -0700

    update examples

---
+++ example/curvedsky_reconstruction/lens_reconstruction_EB-iter.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # A reconstruction nosie dependence on CMB white noise level
+# This code compute normalization and power spectrum of quadratic estiamtors with varying CMB noise level
+
+# In[1]:
+
+
+# external
+import numpy as np
+from matplotlib.pyplot import *
+# from cmblensplus/wrap/
+import basic
+import curvedsky as cs
+# from cmblensplus/utils/
+import plottools as plc
+
+
+# First define parameters
+
+# In[3]:
+
+
+Tcmb  = 2.726e6    # CMB temperature
+Lmin, Lmax  = 2, 4096       # maximum multipole of output normalization
+rlmin, rlmax = 100, 4096  # CMB multipole range for reconstruction
+L = np.linspace(0,Lmax,Lmax+1)
+Lfac = (L*(L+1.))**2/(2*np.pi)
+ac2rad = np.pi/10800.
+
+
+# Load arrays of CMB unlensed and lensed Cls. Unlensed Cls are not used for now. The Cls should not be multiplied by any factors and should not have units.  
+
+# In[4]:
+
+
+# ucl is an array of shape [0:5,0:rlmax+1] and ucl[0,:] = TT, ucl[1,:] = EE, ucl[2,:] = TE, lcl[3,:] = phiphi, lcl[4,:] = Tphi
+ucl = basic.aps.read_cambcls('../data/unlensedcls.dat',2,rlmax,5)/Tcmb**2 # TT, EE, TE, pp, Tp
+# lcl is an array of shape [0:4,0:rlmax+1] and lcl[0,:] = TT, lcl[1,:] = EE, lcl[2,:] = BB, and lcl[3,:] = TE
+lcl = basic.aps.read_cambcls('../data/lensedcls.dat',2,rlmax,4,bb=True)/Tcmb**2 # TT, EE, BB, TE
+
+
+# Loop over SNR calculation
+
+# In[ ]:
+
+
+sigs = [5.,3.,1.,.5,.1,.05]
+sigs = [5.]
+snr = np.zeros(len(sigs))
+for i, sig in enumerate(sigs):
+    nl  = np.zeros((4,rlmax+1))
+    nl[0,:] = (sig*ac2rad/Tcmb)**2
+    nl[1,:] = 2*nl[0,:]
+    nl[2,:] = 2*nl[0,:]
+    ocl = lcl + nl
+    Ag, __ = cs.norm_lens.qeb_iter(Lmax,rlmax,rlmin,rlmax,rlmin,rlmax,lcl[1,:],ocl[1,:],ocl[2,:],ucl[3,:])
+
+
+# In[ ]:
+
+
+
+
