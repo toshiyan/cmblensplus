1c12dfc8ee6856229022d742e72219810fa99fe3
Your Name <you@example.com>
2020-12-05 15:26:32 -0800

    update cinv code

--- curvedsky/src/cninv.f90
+++ curvedsky/src/cninv.f90
@@ -30,15 +30,16 @@ subroutine cnfilter_freq(n,mn,npix,lmax,cl,bl,iNcov,maps,xlm,chn,lmaxs,nsides,it
 !*    :maps[n,mn,pix] (double) : Input T, Q, U maps, with bouds (0:n-1,0:mn-1,0:npix-1)
 !*
 !* Args(optional):
-!*    :chn (int) : number of grids for preconsitioner (chn=1 for diagonal preconditioner, default)
+!*    :chn (int) : Number of grids for preconsitioner (chn=1 for diagonal preconditioner, default)
 !*    :lmaxs[chain] (int) : Maximum multipole(s) at each preconditioning and lmaxs[0] is the input maximum multipole of cl
 !*    :nsides[chain] (int) : Nside(s) of preconditoner and nsides[0] should be consistent with the input map's nside. 
-!*    :eps[chain] (double): Numerical parameter to finish the iteration if ave(\|Ax-b\|)<eps, default to 1e-6
-!*    :itns[chain] (int) : Number of interation(s)
+!*    :eps[chain] (double): Parameter to finish the iteration (i.e. terminate if the residul fraction becomes smaller than eps). Default to 1e-6.
+!*    :itns[chain] (int) : Number of interations. 
 !*    :filter (str): C-inverse ('') or Wiener filter (W), default to C-inverse.
-!*    :inl[n,mn,l] (double) : inverse noise spectrum for non white noise case.
+!*    :inl[n,mn,l] (double) : Inverse noise spectrum, 0 for non white noise case.
 !*    :verbose (bool): Output messages, default to False
-!*    :stat (str): Realtime status filename, default to no output file
+!*    :ro (int): the residual fraction is output for every ro iteration (e.g. ro=2 means 1 output per 2 iterations). Default to 50. Useful for convergence speed. 
+!*    :stat (str): Realtime status filename which contains the residual fraction, default to no output file
 !*
 !* Returns:
 !*    :xlm[n,l,m] (dcmplx) : C-inverse / Wiener filtered multipoles, with bounds (0:n-1,0:lmax,0:lmax)
@@ -76,6 +77,7 @@ subroutine cnfilter_freq(n,mn,npix,lmax,cl,bl,iNcov,maps,xlm,chn,lmaxs,nsides,it
   !replace
   !chargs :: npix -> nside
   !add2py :: npix = 12*nside**2
+  !add2py :: if inl==0: inl = 0*iNcov[:,:,:lmax+1]
 
   mnmaxs = mn
 
@@ -194,15 +196,17 @@ subroutine cnfilter_freq_nside(n,mn0,mn1,npix0,npix1,lmax,cl,bl0,bl1,iNcov0,iNco
 !*    :maps0/1[n,mn,pix] (double) : Input T, Q, U maps, with bouds (0:n-1,0:npix-1)
 !*
 !* Args(optional):
-!*    :chn (int) : number of grids for preconsitioner (chn=1 for diagonal preconditioner, default)
+!*    :chn (int) : Number of grids for preconsitioner (chn=1 for diagonal preconditioner, default)
 !*    :lmaxs[chain] (int) : Maximum multipole(s) at each preconditioning and lmaxs[0] is the input maximum multipole of cl
 !*    :nsides0/1[chain] (int) : Nside(s) of preconditoner and nsides[0] should be consistent with the input map's nside. 
-!*    :eps[chain] (double): Numerical parameter to finish the iteration if ave(\|Ax-b\|)<eps, default to 1e-6
-!*    :itns[chain] (int) : Number of interation(s)
+!*    :eps[chain] (double): Parameter to finish the iteration (i.e. terminate if the residul fraction becomes smaller than eps). Default to 1e-6.
+!*    :itns[chain] (int) : Number of interations. 
 !*    :filter (str): C-inverse ('') or Wiener filter (W), default to C-inverse.
-!*    :verbose (bool): Output messages
+!*    :inl[n,mn,l] (double) : Inverse noise spectrum, 0 for non white noise case.
+!*    :verbose (bool): Output messages, default to False
+!*    :ro (int): the residual fraction is output for every ro iteration (e.g. ro=2 means 1 output per 2 iterations). Default to 50. Useful for convergence speed. 
+!*    :stat (str): Realtime status filename which contains the residual fraction, default to no output file
 !*    :reducmn (int): Reducing number of maps per chain (1,2) or not (0, default). If 1, the maps are combined for the same nside inside the multigrid chain. If 2, in addition to the procedure of 1, the each nside maprs are further combined into a single map inside the second chain (chain>=3).
-!*    :stat (str): Realtime status filename
 !*
 !* Returns:
 !*    :xlm[n,l,m] (dcmplx) : C-inverse / Wiener filtered multipoles, with bounds (0:n-1,0:lmax,0:lmax)
@@ -249,6 +253,7 @@ subroutine cnfilter_freq_nside(n,mn0,mn1,npix0,npix1,lmax,cl,bl0,bl1,iNcov0,iNco
   !chargs :: npix1 -> nside1
   !add2py :: npix0 = 12*nside0**2
   !add2py :: npix1 = 12*nside1**2
+  !add2py :: if inl==0: inl = 0*iNcov[:,:,:lmax+1]
 
   mn = mn0 + mn1
   mnmaxs = mn
