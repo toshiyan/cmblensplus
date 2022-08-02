285c5f985049a5c64dd4345750b0a2621eab2839
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2022-08-01 22:45:43 -0700

    no longer use F90 library for binning

--- utils/binning.py
+++ utils/binning.py
@@ -1,12 +1,15 @@
 import numpy as np
 import sys
-import basic
+#import basic
 
 #////////// Multipole binning //////////
 
 class multipole_binning:
 
-    def __init__(self,n,spc='',lmin=1,lmax=2048):
+    def __init__(self,n,spc='',lmin=1,lmax=2048,lstart=0):
+        '''
+        lstart: multipole of cl[:,0], default to 0
+        '''
         
         if not isinstance(n,int): 
             sys.exit('n is not integer')
@@ -18,9 +21,46 @@ class multipole_binning:
         self.spc = spc
         self.lmin = lmin
         self.lmax = lmax
-        self.bp, self.bc = basic.aps.binning(n,[lmin,lmax],spc=spc)
+        self.lstart = lstart
+        self.imax = self.lmax - self.lstart  # index of l=lmax
+        #self.bp, self.bc = basic.aps.binning(n,[lmin,lmax],spc=spc)
+        self.bp, self.bc = binned_ells(n,lmin,lmax,spc)
 
 
+def binned_ells(n,lmin,lmax,spc):
+    '''
+    return binned multipole edges and centers
+    '''
+
+    if lmin<=0 and spc!='':
+        sys.exit('ell minimum should be > 0 for spacing')
+
+    if spc == '':
+        dl = (lmax-lmin)/n
+        bp = np.array( [ lmin+dl*i for i in range(n+1) ] )
+
+    if spc == 'log':
+        dl = np.log(lmax/lmin)/n
+        bp = np.array( [ lmin*np.exp(dl*i) for i in range(n+1) ] )
+    
+    if spc == 'log10':
+        dl = np.log10(lmax/lmin)/n
+        bp = np.array( [ lmin*10.**(dl*i) for i in range(n+1) ] )
+    
+    if spc == 'p2':
+        dl = (np.sqrt(lmax)-np.sqrt(lmin))/n
+        bp = np.array( [ (np.sqrt(lmin)+dl*i)**2 for i in range(n+1) ] )
+    
+    if spc == 'p3':
+        dl = (lmax**(1./3.)-lmin**(1./3.))/n
+        bp = np.array( [ (lmin**3+dl*i)**3 for i in range(n+1) ] )
+    
+    bc = np.array( [ (bp[i]+bp[i-1])/2. for i in range(1,n+1)] ) 
+    
+    return bp, bc
+    
+        
+        
 def binning(cl,mb0,mb1=None,vl=None):
 
     if vl is None:
@@ -35,20 +75,20 @@ def binning(cl,mb0,mb1=None,vl=None):
 
 def binning_opt(cl,vl,mb):
 
-    if mb.lmax > np.shape(cl)[-1] - 1:
-        sys.exit('size of mb.lmax is wrong: '+str(mb.lmax)+', '+str(np.shape(cl)[-1]-1))
+    if mb.imax+1 > np.shape(cl)[-1]:
+        sys.exit('Multipole number of cl should be equal to or larger than '+str(mb.imax)+', while the input cl has a multipole size of '+str(np.shape(cl)[-1]-1))
 
     if np.ndim(cl) == 1:
-        cb = binning_opt_core(cl[:mb.lmax+1],vl[:mb.lmax+1],mb)
+        cb = binning_opt_core(cl[:mb.imax+1],vl[:mb.imax+1],mb)
 
     if np.ndim(cl) == 2:
         snmax = np.shape(cl)[0]
-        cb = np.array([ binning_opt_core(cl[i,:mb.lmax+1],vl[:mb.lmax+1],mb) for i in range(snmax)])
+        cb = np.array([ binning_opt_core(cl[i,:mb.imax+1],vl[:mb.imax+1],mb) for i in range(snmax)])
     
     if np.ndim(cl) == 3:
         snmax = np.shape(cl)[0]
         clnum = np.shape(cl)[1]
-        cb = np.array([[ binning_opt_core(cl[i,c,:mb.lmax+1],vl[:mb.lmax+1],mb) for c in range(clnum)] for i in range(snmax)])
+        cb = np.array([[ binning_opt_core(cl[i,c,:mb.imax+1],vl[:mb.imax+1],mb) for c in range(clnum)] for i in range(snmax)])
 
     return cb
 
@@ -59,7 +99,7 @@ def binning_opt_core(cl,vl,mb):
     
     for i in range(mb.n):
         
-        b0, b1 = int(mb.bp[i]) , int(mb.bp[i+1])
+        b0, b1 = int(mb.bp[i])-mb.lstart , int(mb.bp[i+1])-mb.lstart
         
         cs = cl[b0:b1]
         wl = 1./vl[b0:b1]**2
@@ -78,7 +118,7 @@ def binning_opt_weight(vl,mb):
     
     for i in range(mb.n):
         
-        b0, b1 = int(mb.bp[i]) , int(mb.bp[i+1])
+        b0, b1 = int(mb.bp[i])-mb.lstart , int(mb.bp[i+1])-mb.lstart
         
         wl = 1./vl[b0:b1]**2
         
@@ -90,39 +130,50 @@ def binning_opt_weight(vl,mb):
     return wb
 
 
-def binning1(cl,b):
+def binning1(cl,mb):
     """
     dim = 1  ->  cl = [L] and cb = [b]
     dim > 1  ->  cl = [...,L] and cb = [...,b]
     """
+    
+    if mb.imax+1 > np.shape(cl)[-1]:
+        sys.exit('Multipole number of cl should be equal to or larger than '+str(mb.imax)+', while the input cl has a multipole size of '+str(np.shape(cl)[-1]-1))
 
-    if b.lmax > np.shape(cl)[-1] - 1:
-        sys.exit('size of b.lmax is wrong: '+str(b.lmax)+', '+str(np.shape(cl)[-1]-1))
+    vl = np.ones((mb.imax+1))
 
     if np.ndim(cl) == 1:
-        cb = basic.aps.cl2bcl(b.n,b.lmax,cl[:b.lmax+1],lmin=b.lmin,spc=b.spc)
+        #aps = np.zeros(mb.lmax)
+        #aps[mb.lmin:mb.lmax+1] = cl[:mb.imax+1]
+        #cb = basic.aps.cl2bcl(mb.n,mb.lmax,aps,lmin=mb.lmin,spc=mb.spc)
+        cb = binning_opt_core(cl[:mb.imax+1],vl,mb)
 
     if np.ndim(cl) == 2:
         snmax = np.shape(cl)[0]
-        cb = np.array([basic.aps.cl2bcl(b.n,b.lmax,cl[i,:b.lmax+1],lmin=b.lmin,spc=b.spc) for i in range(snmax)])
+        #aps = np.zeros((snmax,mb.lmax))
+        #aps[:,mb.lmin:mb.lmax+1] = cl[:,:mb.imax+1]
+        #cb = np.array([basic.aps.cl2bcl(mb.n,mb.lmax,aps[i,:mb.lmax+1],lmin=mb.lmin,spc=mb.spc) for i in range(snmax)])
+        cb = np.array([ binning_opt_core(cl[i,:mb.imax+1],vl,mb) for i in range(snmax)])
 
     if np.ndim(cl) == 3:
         snmax = np.shape(cl)[0]
         clnum = np.shape(cl)[1]
-        cb = np.array([[basic.aps.cl2bcl(b.n,b.lmax,cl[i,c,:b.lmax+1],lmin=b.lmin,spc=b.spc) for c in range(clnum)] for i in range(snmax)])
+        #aps = np.zeros((snmax,clnum,mb.lmax))
+        #aps[:,:,mb.lmin:mb.lmax+1] = cl[:,:,:mb.imax+1]
+        #cb = np.array([[basic.aps.cl2bcl(mb.n,mb.lmax,aps[i,c,:mb.lmax+1],lmin=mb.lmin,spc=mb.spc) for c in range(clnum)] for i in range(snmax)])
+        cb = np.array([[ binning_opt_core(cl[i,c,:mb.imax+1],vl,mb) for c in range(clnum)] for i in range(snmax)])
 
     return cb
 
 
-def binning2(cl,b0,b1):
+def binning2(cl,mb0,mb1):
 
-    if b1.lmin != b0.lmax+1:
+    if mb1.lmin != mb0.lmax+1:
         sys.exit('wrong split')
-    if b1.lmax > np.shape(cl)[-1]-1:
+    if mb1.lmax > np.shape(cl)[-1]-1:
         sys.exit('wrong lmax')
 
-    cb0 = binning1(cl,b0)
-    cb1 = binning1(cl,b1)
+    cb0 = binning1(cl,mb0)
+    cb1 = binning1(cl,mb1)
     if np.ndim(cl) == 1:
         return np.concatenate((cb0,cb1))
     if np.ndim(cl) == 2:
