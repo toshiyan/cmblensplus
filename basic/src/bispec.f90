ab17febee717eb61e5998bdda56d8080a8f2a6eb
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2023-05-19 02:41:50 -0700

    major update

--- basic/src/bispec.f90
+++ basic/src/bispec.f90
@@ -12,7 +12,7 @@ module bispec
 contains
 
 
-subroutine cl_flat(cpmodel,z,dz,zs,lmax,k,pk0,zn,kn,cl,pktype,cltype,dNdz)
+subroutine cl_flat(cpmodel,z,dz,zs,lmax,k,pk0,zn,kn,cl,pktype,cltype,dNdz,wdel)
 !*  Compute flat-sky lensing powerspectrum analytically
 !* 
 !*  Args:
@@ -28,6 +28,7 @@ subroutine cl_flat(cpmodel,z,dz,zs,lmax,k,pk0,zn,kn,cl,pktype,cltype,dNdz)
 !*    :pktype (str) : fitting formula for the matter power spectrum (Lin, S02 or T12)
 !*    :cltype (str) : kk, gk, or gg
 !*    :dNdz[zn] (double) : redshift distribution of galaxy, only used when cltype includes g
+!*    :wdel[zn,l] (double) : modified chi-kernel function for z-cleaning at l=0 to lmax
 !*
 !*  Returns:
 !*    :cl[l] (double) : power spectrum from LSS contributions at [lmin,lmax]
@@ -39,10 +40,11 @@ subroutine cl_flat(cpmodel,z,dz,zs,lmax,k,pk0,zn,kn,cl,pktype,cltype,dNdz)
   double precision, intent(in), dimension(2) :: zs
   double precision, intent(in), dimension(1:zn) :: z, dz, dNdz
   double precision, intent(in), dimension(1:kn) :: k, pk0
+  double precision, intent(in), dimension(1:zn,0:lmax) :: wdel
   double precision, intent(out), dimension(0:lmax) :: cl
   !internal
   type(cosmoparams) :: cp
-  double precision :: pl(zn,lmax), zker(zn)
+  double precision :: pl(zn,lmax), zker(zn), weight(2,zn,lmax)
   !opt4py :: zn = 0
   !opt4py :: kn = 0
   !add2py :: if zn == 0: zn=len(z)
@@ -51,6 +53,8 @@ subroutine cl_flat(cpmodel,z,dz,zs,lmax,k,pk0,zn,kn,cl,pktype,cltype,dNdz)
   !opt4py :: cltype = 'kk'
   !opt4py :: dNdz = None
   !add2py :: if dNdz is None: dNdz = z*0.
+  !opt4py :: wdel = None
+  !add2py :: if wdel is None: wdel = numpy.zeros((zn,lmax+1))
   !add2py :: if len(dNdz) != zn: print('size of dNdz is strange')
 
   ! cosmological parameters (to compute background quantities)
@@ -58,17 +62,16 @@ subroutine cl_flat(cpmodel,z,dz,zs,lmax,k,pk0,zn,kn,cl,pktype,cltype,dNdz)
 
   ! precompute quantities for power spectrum
   call get_pl(cp,z,k*cp%h,pk0/cp%h**3,lmax,pktype,pl)
-  call cl_zweight(cp,z,dz,zs,zker,cltype,dNdz)
+  call cl_zweight(cp,z,dz,zs,dNdz,wdel(:,1:),cltype,zker,weight)
 
-  ! power pectrum
-  write(*,*) 'compute power pectrum'
+  ! commpute power pectrum
   cl = 0d0
-  call cl_calc_flat(pl,zker,cl(1:))
+  call cl_calc_flat(pl,zker,weight,cl(1:))
 
 end subroutine cl_flat
 
 
-subroutine bispeclens(shap,cpmodel,model,z,dz,zs,lmin,lmax,k,pk0,lan,kan,bl0,bl1,zn,kn,pktype,ltype,btype,dNdz)
+subroutine bispeclens(shap,cpmodel,model,z,dz,zs,lmin,lmax,k,pk0,lan,kan,bl0,bl1,zn,kn,pktype,ltype,btype,dNdz,wdel)
 !*  Compute lensing bispectrum analytically
 !* 
 !*  Args:
@@ -88,6 +91,7 @@ subroutine bispeclens(shap,cpmodel,model,z,dz,zs,lmin,lmax,k,pk0,lan,kan,bl0,bl1
 !*    :ltype (str) : fullsky correction (full) or not 
 !*    :btype (str) : bispectrum type, i.e., kkk (lens-lens-lens), gkk (density-lens-lens), ggk (density-density-lens), or ggg (density-density-density)
 !*    :dNdz[zn] (double) : redshift distribution of galaxy, only used when btype includes g
+!*    :wdel[zn,l] (double) : modified chi-kernel function by z-cleaning at l=0 to lmax
 !*
 !*  Returns:
 !*    :bl0[l] (double) : lensing bispectrum from LSS contributions at [lmin,lmax]
@@ -101,10 +105,11 @@ subroutine bispeclens(shap,cpmodel,model,z,dz,zs,lmin,lmax,k,pk0,lan,kan,bl0,bl1
   double precision, intent(in) :: lan, kan
   double precision, intent(in), dimension(1:zn) :: z, dz, dNdz
   double precision, intent(in), dimension(1:kn) :: k, pk0
+  double precision, intent(in), dimension(1:zn,0:lmax) :: wdel
   double precision, intent(out), dimension(lmin:lmax) :: bl0, bl1
   !internal
   integer :: l0, oL(2), i
-  double precision, allocatable :: wp(:,:,:,:), ck(:,:,:)
+  double precision, allocatable :: wp(:,:,:), wck(:,:,:,:), wdel_tmp(:,:)
   type(gauss_legendre_params) :: gl
   type(cosmoparams) :: cp
   type(bispecfunc)  :: b
@@ -119,6 +124,8 @@ subroutine bispeclens(shap,cpmodel,model,z,dz,zs,lmin,lmax,k,pk0,lan,kan,bl0,bl1
   !opt4py :: btype = 'kkk'
   !opt4py :: dNdz = None
   !add2py :: if dNdz is None: dNdz = z*0.
+  !opt4py :: wdel = None
+  !add2py :: if wdel is None: wdel = numpy.zeros((zn,lmax+1))
   !add2py :: if len(dNdz) != zn: print('size of dNdz is strange')
 
   ! cosmological parameters (to compute background quantities)
@@ -129,10 +136,13 @@ subroutine bispeclens(shap,cpmodel,model,z,dz,zs,lmin,lmax,k,pk0,lan,kan,bl0,bl1
   oL = (/lmin,lmax/)
 
   ! precompute quantities for bispectrum
-  allocate(wp(3,3,zn,lmax),ck(3,zn,lmax))
-  call bispec_lens_lss_init(cp,b,z,dz,zs,k*cp%h,pk0/cp%h**3,oL,model,pktype,btype) !correction for h/Mpc to /Mpc
-  call bispec_lens_pb_init(cp,b%kl,b%pl,z,dz,zs,oL,wp,ck) !only compute for btype=kkk
-  if (btype/='kkk')  call gal_zweight(btype,b%zker,dNdz)
+  allocate(wp(3,zn,oL(2)),wck(3,3,zn,oL(2)),wdel_tmp(zn,1:lmax))
+  wdel_tmp = wdel(:,1:)
+  call bispec_lens_lss_init(cp,b,z,dz,zs,k*cp%h,pk0/cp%h**3,oL,dNdz,wdel_tmp,model,pktype,btype) !correction for h/Mpc to /Mpc
+  call bispec_lens_pb_init(cp,b%kl,b%pl,z,dz,zs,oL,b%weight,wp,wck) !only compute for btype=kkk
+  deallocate(wdel_tmp)
+  write(*,*) 'A'
+  !if (btype/='kkk')  call gal_zweight(btype,b%zker,dNdz)
 
   ! MG parameter z-evolution
   allocate(b%mgp(2,zn));  b%mgp=1d0
@@ -146,12 +156,14 @@ subroutine bispeclens(shap,cpmodel,model,z,dz,zs,lmin,lmax,k,pk0,lan,kan,bl0,bl1
   l0 = lmin+(lmax-lmin)*0.5d0/20d0
   if (mod(l0,2)/=0) l0 = l0+1
   call bispec_lens_lss(cp,b,shap,oL,model,ltype,l0,bl0)
-  call bispec_lens_pb(shap,oL,wp,ck,bl1,l0,ltype)
+  call bispec_lens_pb(shap,oL,wp,wck,bl1,l0,ltype)
+
+  deallocate(b%mgp,wp,wck)
 
 end subroutine bispeclens
 
 
-subroutine bispeclens_bin(shap,cpmodel,model,z,dz,zn,zs,lmin,lmax,bn,k,pk0,kn,lan,kan,bc,bl0,bl1,pktype,btype,dNdz)
+subroutine bispeclens_bin(shap,cpmodel,model,z,dz,zn,zs,lmin,lmax,bn,k,pk0,kn,lan,kan,bc,bl0,bl1,pktype,btype,dNdz,wdel)
 !*  Compute binned lensing bispectrum analytically
 !* 
 !*  Args:
@@ -171,6 +183,7 @@ subroutine bispeclens_bin(shap,cpmodel,model,z,dz,zn,zs,lmin,lmax,bn,k,pk0,kn,la
 !*    :pktype (str) : fitting formula for the matter power spectrum (Lin, S02 or T12)
 !*    :btype (str) : bispectrum type, i.e., kkk (lens-lens-lens), gkk (density-lens-lens), ggk (density-density-lens), or ggg (density-density-density)
 !*    :dNdz[zn] (double) : redshift distribution of galaxy, only used when btype includes g
+!*    :wdel[zn,l] (double) : modified chi-kernel function by z-cleaning at l=0 to lmax
 !*
 !*  Returns:
 !*    :bc[bn] (double)  : multipole bin centers
@@ -183,8 +196,9 @@ subroutine bispeclens_bin(shap,cpmodel,model,z,dz,zn,zs,lmin,lmax,bn,k,pk0,kn,la
   integer, intent(in) :: lmin, lmax, zn, bn, kn
   double precision, intent(in), dimension(3) :: zs
   double precision, intent(in) :: lan, kan
-  double precision, intent(in),  dimension(1:zn) :: z, dz, dNdz
-  double precision, intent(in),  dimension(1:kn) :: k, pk0
+  double precision, intent(in), dimension(1:zn) :: z, dz, dNdz
+  double precision, intent(in), dimension(1:kn) :: k, pk0
+  double precision, intent(in), dimension(1:zn,0:lmax) :: wdel
   double precision, intent(out), dimension(1:bn) :: bc, bl0, bl1
   !opt4py :: zn = 0
   !opt4py :: kn = 0
@@ -196,11 +210,13 @@ subroutine bispeclens_bin(shap,cpmodel,model,z,dz,zn,zs,lmin,lmax,bn,k,pk0,kn,la
   !opt4py :: btype = 'kkk'
   !opt4py :: dNdz = None
   !add2py :: if dNdz is None: dNdz = z*0.
+  !opt4py :: wdel = None
+  !add2py :: if wdel is None: wdel = numpy.zeros((zn,lmax+1))
   !add2py :: if len(dNdz) != zn: print('size of dNdz is strange')
 
   !internal
   integer :: oL(2), l, i, eL1(2), eL2(2), eL3(2)
-  double precision, allocatable :: bp(:), wp(:,:,:,:), ck(:,:,:)
+  double precision, allocatable :: bp(:), wp(:,:,:), wck(:,:,:,:)
   type(gauss_legendre_params) :: gl
   type(cosmoparams) :: cp
   type(bispecfunc)  :: b
@@ -212,10 +228,10 @@ subroutine bispeclens_bin(shap,cpmodel,model,z,dz,zn,zs,lmin,lmax,bn,k,pk0,kn,la
   oL = (/lmin,lmax/)
 
   ! precompute quantities for bispectrum
-  allocate(wp(3,3,zn,oL(2)),ck(3,zn,oL(2)))
-  call bispec_lens_lss_init(cp,b,z,dz,zs,k*cp%h,pk0/cp%h**3,oL,model,pktype) !correction for h/Mpc to /Mpc
-  call bispec_lens_pb_init(cp,b%kl,b%pl,z,dz,zs,oL,wp,ck)
-  if (btype/='kkk')  call gal_zweight(btype,b%zker,dNdz)
+  allocate(wp(3,zn,oL(2)),wck(3,3,zn,oL(2)))
+  call bispec_lens_lss_init(cp,b,z,dz,zs,k*cp%h,pk0/cp%h**3,oL,dNdz,wdel(:,1:),model,pktype) !correction for h/Mpc to /Mpc
+  call bispec_lens_pb_init(cp,b%kl,b%pl,z,dz,zs,oL,b%weight,wp,wck) !only compute for btype=kkk
+  !if (btype/='kkk')  call gal_zweight(btype,b%zker,dNdz)
 
   ! MG parameter z-evolution
   allocate(b%mgp(2,zn));  b%mgp=1d0
@@ -250,13 +266,15 @@ subroutine bispeclens_bin(shap,cpmodel,model,z,dz,zn,zs,lmin,lmax,bn,k,pk0,kn,la
       eL3 = int(bp(bn/2:bn/2+1))
     end select
     write(*,*) eL1, eL2, eL3
-    call bispec_lens_bin(cp,b,eL1,eL2,eL3,wp,ck,model,bl0(l),bl1(l))
+    call bispec_lens_bin(cp,b,eL1,eL2,eL3,wp,wck,model,bl0(l),bl1(l))
   end do
+
+  deallocate(b%mgp,bp,wp,wck)
 
 end subroutine bispeclens_bin
 
 
-subroutine bispeclens_snr(cpmodel,model,z,dz,zn,zs,lmin,lmax,cl,k,pk0,kn,snr,pktype,btype,dNdz,cgg,ro)
+subroutine bispeclens_snr(cpmodel,model,z,dz,zn,zs,lmin,lmax,cl,k,pk0,kn,snr,pktype,btype,dNdz,cgg,ro,wdel)
 !*  Compute SNR of lensing bispectrum analytically
 !* 
 !*  Args:
@@ -266,7 +284,7 @@ subroutine bispeclens_snr(cpmodel,model,z,dz,zn,zs,lmin,lmax,cl,k,pk0,kn,snr,pkt
 !*    :dz[zn] (double) : interval of z
 !*    :zs[3] (double) : source redshifts
 !*    :lmin/lmax (int) : minimum/maximum multipoles of the bispectrum
-!*    :cl[l] (int) : observed lensing spectrum at 0<=l<=lmax
+!*    :cl[l] (int) : observed angular power spectrum at 0<=l<=lmax
 !*    :k[kn] (double) : k for the matter power spectrum in unit of [h/Mpc]
 !*    :pk0[kn] (double) : the linear matter power spectrum at z=0 in unit of [Mpc^3/h^3]
 !*
@@ -274,11 +292,12 @@ subroutine bispeclens_snr(cpmodel,model,z,dz,zn,zs,lmin,lmax,cl,k,pk0,kn,snr,pkt
 !*    :pktype (str) : fitting formula for the matter power spectrum (Lin, S02 or T12)
 !*    :btype (str) : bispectrum type, i.e., kkk (lens-lens-lens), gkk (density-lens-lens), ggk (density-density-lens), or ggg (density-density-density)
 !*    :dNdz[zn] (double) : redshift distribution of galaxy, only used when btype includes g
+!*    :wdel[zn,l] (double) : modified chi-kernel function by z-cleaning at l=0 to lmax
 !*    :cgg[l] (double) : observed galaxy spectrum
 !*    :ro (int) : output progress for every "ro" multipoles (ro=100, default)
 !*
 !*  Returns:
-!*    :snr (double) : total SNR
+!*    :snr[2] (double) : total SNR amd LSS-only SNR
 !*
   implicit none
   !I/O
@@ -288,7 +307,8 @@ subroutine bispeclens_snr(cpmodel,model,z,dz,zn,zs,lmin,lmax,cl,k,pk0,kn,snr,pkt
   double precision, intent(in), dimension(1:zn) :: z, dz, dNdz
   double precision, intent(in), dimension(1:kn) :: k, pk0
   double precision, intent(in), dimension(0:lmax) :: cl, cgg
-  double precision, intent(out) :: snr
+  double precision, intent(in), dimension(1:zn,0:lmax) :: wdel
+  double precision, intent(out), dimension(1:2) :: snr
   !opt4py :: zn = 0
   !opt4py :: kn = 0
   !add2py :: if zn == 0: zn=len(z)
@@ -298,6 +318,8 @@ subroutine bispeclens_snr(cpmodel,model,z,dz,zn,zs,lmin,lmax,cl,k,pk0,kn,snr,pkt
   !opt4py :: dNdz = None
   !add2py :: if dNdz is None: dNdz = z*0.
   !add2py :: if len(dNdz) != zn: print('size of dNdz is strange')
+  !opt4py :: wdel = None
+  !add2py :: if wdel is None: wdel = numpy.zeros((zn,lmax+1))
   !opt4py :: cgg = None
   !opt4py :: ro = 100
   !add2py :: if cgg is None: cgg = cl*0.
@@ -305,7 +327,7 @@ subroutine bispeclens_snr(cpmodel,model,z,dz,zn,zs,lmin,lmax,cl,k,pk0,kn,snr,pkt
 
   !internal
   integer :: eL(2)
-  double precision, allocatable :: wp(:,:,:,:), ck(:,:,:)
+  double precision, allocatable :: wp(:,:,:), wck(:,:,:,:)
   type(cosmoparams) :: cp
   type(bispecfunc)  :: b
 
@@ -316,21 +338,21 @@ subroutine bispeclens_snr(cpmodel,model,z,dz,zn,zs,lmin,lmax,cl,k,pk0,kn,snr,pkt
   eL = (/lmin,lmax/)
 
   ! precompute quantities for bispectrum
-  allocate(wp(3,3,zn,eL(2)),ck(3,zn,eL(2)))
-  call bispec_lens_lss_init(cp,b,z,dz,zs,k*cp%h,pk0/cp%h**3,eL,model,pktype,btype) !correction for h/Mpc to /Mpc
-  call bispec_lens_pb_init(cp,b%kl,b%pl,z,dz,zs,eL,wp,ck)
-  if (btype/='kkk')  call gal_zweight(btype,b%zker,dNdz)
+  allocate(wp(3,zn,eL(2)),wck(3,3,zn,eL(2)))
+  call bispec_lens_lss_init(cp,b,z,dz,zs,k*cp%h,pk0/cp%h**3,eL,dNdz,wdel(:,1:),model,pktype,btype) !correction for h/Mpc to /Mpc
+  call bispec_lens_pb_init(cp,b%kl,b%pl,z,dz,zs,eL,b%weight,wp,wck) !only compute for btype=kkk
+  !if (btype/='kkk')  call gal_zweight(btype,b%zker,dNdz)
 
   ! snr
   allocate(b%mgp(2,zn));  b%mgp=1d0
   write(*,*) 'compute bispectrum snr'
   select case(btype)
   case('kkk','ggg')
-    call bispec_lens_snr(cp,b,eL,cl(1:lmax),wp,ck,model,ro,snr)
+    call bispec_lens_snr(cp,b,eL,cl(1:lmax),wp,wck,model,ro,snr)
   case('gkk','ggk')
-    call bispec_lens_snr_cross(cp,b,eL,cgg(1:lmax),cl(1:lmax),btype,model,ro,snr)
+    call bispec_lens_snr_cross(cp,b,eL,cgg(1:lmax),cl(1:lmax),btype,model,ro,snr(1))
   end select
-  deallocate(b%mgp,wp,ck)
+  deallocate(b%mgp,wp,wck)
 
 end subroutine bispeclens_snr
 
@@ -421,7 +443,7 @@ subroutine zpoints(zmin,zmax,zn,z,dz,zspace)
 end subroutine zpoints
 
 
-subroutine skewspeclens(cpmodel,model,zmin,zmax,zn,zs,bn,ols,lmin,lmax,k,pk0,kn,skew,theta,pktype,pb,Om,H0,w0,wa,mnu,ns,verbose)
+subroutine skewspeclens(cpmodel,model,zmin,zmax,zn,zs,bn,ols,lmin,lmax,k,pk0,kn,skew,theta,pktype,pb,Om,H0,w0,wa,mnu,ns,verbose,wdel)
 !* Compute skew spectrum using a matter bispectrum fitting formula
 !*
 !*  Args:
@@ -440,6 +462,7 @@ subroutine skewspeclens(cpmodel,model,zmin,zmax,zn,zs,bn,ols,lmin,lmax,k,pk0,kn,
 !*    :theta (double) : kappa map resolution in arcmin
 !*    :pb (bool) : with post-Born correction or not (default=True)
 !*    :verbose (bool) : output messages
+!*    :wdel[zn,l] (double) : modified chi-kernel function by z-cleaning at l=0 to lmax
 !*
 !*  Returns:
 !*    :skew (double)  : skew-spectrum
@@ -453,6 +476,7 @@ subroutine skewspeclens(cpmodel,model,zmin,zmax,zn,zs,bn,ols,lmin,lmax,k,pk0,kn,
   double precision, intent(in) :: Om, H0, w0, wa, mnu, ns
   integer, intent(in), dimension(1:bn) :: ols
   double precision, intent(in), dimension(1:kn) :: k, pk0
+  double precision, intent(in), dimension(1:zn,0:lmax) :: wdel
   double precision, intent(out), dimension(1:3,1:bn) :: skew
   logical, intent(in) :: pb, verbose
   !opt4py :: pktype = 'T12'
@@ -469,11 +493,13 @@ subroutine skewspeclens(cpmodel,model,zmin,zmax,zn,zs,bn,ols,lmin,lmax,k,pk0,kn,
   !opt4py :: kn = 0
   !add2py :: if kn == 0: kn=len(k)
   !opt4py :: verbose = True
+  !opt4py :: wdel = None
+  !add2py :: if wdel is None: wdel = numpy.zeros((zn,lmax+1))
   !internal
   integer :: n, eL(2), tL(2)
   double precision :: zss(3)
-  double precision, dimension(1:zn) :: z, dz
-  double precision, allocatable :: wp(:,:,:,:), ck(:,:,:)
+  double precision, dimension(1:zn) :: z, dz, dndz
+  double precision, allocatable :: wp(:,:,:), wck(:,:,:,:)
   type(gauss_legendre_params) :: gl
   type(cosmoparams) :: cp
   type(bispecfunc)  :: b
@@ -500,19 +526,20 @@ subroutine skewspeclens(cpmodel,model,zmin,zmax,zn,zs,bn,ols,lmin,lmax,k,pk0,kn,
   tL(2) = max(lmax,ols(bn))
 
   ! skewspectrum
-  allocate(wp(3,3,zn,tL(2)),ck(3,zn,tL(2)))
+  allocate(wp(3,zn,tL(2)),wck(3,3,zn,tL(2)))
   zss(1) = zs(1)
   zss(2) = zs(2)
   zss(3) = zs(2)
-  call bispec_lens_lss_init(cp,b,z,dz,zss,k*cp%h,pk0/cp%h**3,tL,model,pktype,verbose=verbose) 
-  call bispec_lens_pb_init(cp,b%kl,b%pl,z,dz,zss,tL,wp,ck)
+  dndz = 0d0 ! dummy
+  call bispec_lens_lss_init(cp,b,z,dz,zss,k*cp%h,pk0/cp%h**3,tL,dndz,wdel(:,1:),model,pktype,verbose=verbose) 
+  call bispec_lens_pb_init(cp,b%kl,b%pl,z,dz,zss,tL,b%weight,wp,wck)
   skew = 0d0
   if (verbose) write(*,*) 'calc skewspec'
   do n = 1, bn
-    call skewspec_lens(cp,b,ols(n),(/lmin,lmax/),(/1d0,1d0/),wp,ck,model,theta,skew(:,n),pb)
+    call skewspec_lens(cp,b,ols(n),(/lmin,lmax/),(/1d0,1d0/),wp,wck,model,theta,skew(:,n),pb)
     if (verbose) write(*,*) ols(n), skew(1,n)
   end do
-  deallocate(wp,ck)
+  deallocate(wp,wck)
 
 end subroutine skewspeclens
 
