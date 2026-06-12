4dcd919151190079020a1c81fa706647fa23fb5a
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2026-06-12 10:32:56 -0700

    test

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+
+#conda create -n py_v3_13 -c conda-forge python=3.13 numpy scipy matplotlib astropy healpy ducc0 meson ninja meson-python pkg-config compilers ipykernel pip tqdm
+
 rm -rf build
 make -C fortran_internal/src_dft clean
 make -C fortran_internal/src_utils clean
